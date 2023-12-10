1d0cad0a13349907a922f1af6a03389f6a64beb8
Todd Norton <toddmnorton@live.com>
2023-12-10 16:14:52 -0700

    finished

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
-
+    
+    @IBAction func shakeEightBallButton(_ sender: UIButton) {
+        print("shake me!")
+        
+        imageView.image = ballArray[Int.random(in: 0...4)]
+    }
+    
 }
 
